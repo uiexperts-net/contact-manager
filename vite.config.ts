649d64db800364a2983d22bf4ec5eb2a2fe239ed
Ram L <ram@uiexperts.net>
2024-09-09 13:07:41 +0530

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base :"/contact-picker-app/"
+  base :"/contact-manager/",
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true, // also necessary
+  }
 })
